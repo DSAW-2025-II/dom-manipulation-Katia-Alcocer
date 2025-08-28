0869531fa3fe3483b9f4602d921b73d5d475dc96
Katia-Alcocer <ki.alcoceraguilar@ugto.mx>
2025-08-28 00:22:08 -0600

    add the function to add tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 console.log("JavaScript working!");
+let input = document.querySelector("#taskInput");
+let addButton = document.querySelector("#addButton");
+let taskList = document.querySelector("#DynamicList");
+
+addButton.addEventListener("click", function() {
+    if(input.value.trim() !== "") {
+        let listItem = document.createElement("li");
+        listItem.textContent = input.value;
+        taskList.appendChild(listItem);
+        input.value = "";
+    }
+});
